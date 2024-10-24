680ed41aeede14cd3e23ccb37b13bbec4d007e79
MaksItLab <maksim.ardintsev@mail.ru>
2024-10-24 17:11:05 +0300

    Добавьте файлы проекта.

---
+++ ProcessManipulation/Form1.Designer.cs
@@ -0,0 +1,168 @@
+﻿namespace ProcessManipulation
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.StartButton = new System.Windows.Forms.Button();
+            this.StopButton = new System.Windows.Forms.Button();
+            this.CloseWindowButton = new System.Windows.Forms.Button();
+            this.RefreshButton = new System.Windows.Forms.Button();
+            this.RunNotepadButton = new System.Windows.Forms.Button();
+            this.StartedAssemblies = new System.Windows.Forms.ListBox();
+            this.AvailableAssemblies = new System.Windows.Forms.ListBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.StartedAssemblies);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(311, 426);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Запущенные процессы";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.AvailableAssemblies);
+            this.groupBox2.Location = new System.Drawing.Point(477, 12);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(311, 426);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Доступные сборки";
+            // 
+            // StartButton
+            // 
+            this.StartButton.Location = new System.Drawing.Point(329, 91);
+            this.StartButton.Name = "StartButton";
+            this.StartButton.Size = new System.Drawing.Size(142, 45);
+            this.StartButton.TabIndex = 2;
+            this.StartButton.Text = "Start";
+            this.StartButton.UseVisualStyleBackColor = true;
+            this.StartButton.Click += new System.EventHandler(this.StartButton_Click);
+            // 
+            // StopButton
+            // 
+            this.StopButton.Location = new System.Drawing.Point(329, 142);
+            this.StopButton.Name = "StopButton";
+            this.StopButton.Size = new System.Drawing.Size(142, 45);
+            this.StopButton.TabIndex = 3;
+            this.StopButton.Text = "Stop";
+            this.StopButton.UseVisualStyleBackColor = true;
+            this.StopButton.Click += new System.EventHandler(this.StopButton_Click);
+            // 
+            // CloseWindowButton
+            // 
+            this.CloseWindowButton.Location = new System.Drawing.Point(329, 193);
+            this.CloseWindowButton.Name = "CloseWindowButton";
+            this.CloseWindowButton.Size = new System.Drawing.Size(142, 45);
+            this.CloseWindowButton.TabIndex = 4;
+            this.CloseWindowButton.Text = "Close Window";
+            this.CloseWindowButton.UseVisualStyleBackColor = true;
+            this.CloseWindowButton.Click += new System.EventHandler(this.CloseWindowButton_Click);
+            // 
+            // RefreshButton
+            // 
+            this.RefreshButton.Location = new System.Drawing.Point(329, 244);
+            this.RefreshButton.Name = "RefreshButton";
+            this.RefreshButton.Size = new System.Drawing.Size(142, 45);
+            this.RefreshButton.TabIndex = 5;
+            this.RefreshButton.Text = "Refresh";
+            this.RefreshButton.UseVisualStyleBackColor = true;
+            this.RefreshButton.Click += new System.EventHandler(this.RefreshButton_Click);
+            // 
+            // RunNotepadButton
+            // 
+            this.RunNotepadButton.Location = new System.Drawing.Point(329, 295);
+            this.RunNotepadButton.Name = "RunNotepadButton";
+            this.RunNotepadButton.Size = new System.Drawing.Size(142, 45);
+            this.RunNotepadButton.TabIndex = 6;
+            this.RunNotepadButton.Text = "Run Notepad";
+            this.RunNotepadButton.UseVisualStyleBackColor = true;
+            this.RunNotepadButton.Click += new System.EventHandler(this.RunNotepadButton_Click);
+            // 
+            // StartedAssemblies
+            // 
+            this.StartedAssemblies.FormattingEnabled = true;
+            this.StartedAssemblies.ItemHeight = 16;
+            this.StartedAssemblies.Location = new System.Drawing.Point(6, 21);
+            this.StartedAssemblies.Name = "StartedAssemblies";
+            this.StartedAssemblies.Size = new System.Drawing.Size(298, 388);
+            this.StartedAssemblies.TabIndex = 0;
+            this.StartedAssemblies.SelectedIndexChanged += new System.EventHandler(this.StartedAssemblies_SelectedIndexChanged);
+            // 
+            // AvailableAssemblies
+            // 
+            this.AvailableAssemblies.FormattingEnabled = true;
+            this.AvailableAssemblies.ItemHeight = 16;
+            this.AvailableAssemblies.Location = new System.Drawing.Point(7, 21);
+            this.AvailableAssemblies.Name = "AvailableAssemblies";
+            this.AvailableAssemblies.Size = new System.Drawing.Size(298, 388);
+            this.AvailableAssemblies.TabIndex = 1;
+            this.AvailableAssemblies.SelectedIndexChanged += new System.EventHandler(this.AvailableAssemblies_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.RunNotepadButton);
+            this.Controls.Add(this.RefreshButton);
+            this.Controls.Add(this.CloseWindowButton);
+            this.Controls.Add(this.StopButton);
+            this.Controls.Add(this.StartButton);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Управление дочерними процессами";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.ListBox StartedAssemblies;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.ListBox AvailableAssemblies;
+        private System.Windows.Forms.Button StartButton;
+        private System.Windows.Forms.Button StopButton;
+        private System.Windows.Forms.Button CloseWindowButton;
+        private System.Windows.Forms.Button RefreshButton;
+        private System.Windows.Forms.Button RunNotepadButton;
+    }
+}
+
---
+++ ProcessManipulation/Form1.cs
@@ -0,0 +1,223 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Management;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.Window;
+
+namespace ProcessManipulation
+{
+    public partial class Form1 : Form
+    {
+        //константа, идентифицирующая сообщение WM _ SETTEXT
+        const uint WM_SETTEXT = 0x0C;
+        //импортируем функцию SendMEssage из библиотеки
+        //user32.dll
+        [DllImport("user32.dll")]
+        public static extern IntPtr SendMessage(IntPtr hwnd,
+                                                uint Msg,
+                                                int wParam,
+                                                [MarshalAs(UnmanagedType.LPStr)] string lParam);
+        /*список, в котором будут храниться объекты,
+        * описывающие дочерние процессы приложения*/
+        List<Process> Processes = new List<Process>();
+        /*счётчик запущенных процессов*/
+        int Counter = 0;
+        public Form1()
+        {
+            InitializeComponent();
+            LoadAvailableAssemblies();
+        }
+        void LoadAvailableAssemblies()
+        {
+            //название файла сборки текущего приложения
+            string except = new FileInfo(Application.ExecutablePath).Name;
+            //получаем название файла без расширения
+            except = except.Substring(0, except.IndexOf("."));
+            //получаем все *.exe файлы из домашней
+            //директории
+            string[] files = Directory.GetFiles(Application.StartupPath, "*.exe");
+            foreach (var file in files)
+            {
+                //получаем имя файла
+                string fileName = new FileInfo(file).Name;
+                /*если имя файла не содержит имени исполняемого
+                *файла проекта, то оно добавляется в список*/
+                if (fileName.IndexOf(except) == -1)
+                    AvailableAssemblies.Items.Add(fileName);
+
+            }
+        }
+
+        /*метод, запускающий процесс на исполнение и
+        * сохраняющий объект, который его описывает*/
+        void RunProcess(string AssamblyName)
+        {
+            //запускаем процесс на соновании исполняемого файла
+            Process proc = Process.Start(AssamblyName);
+            //добавляем процесс в список
+            Processes.Add(proc);
+            /*проверяем, стал ли созданный процесс дочерним,
+             *по отношению к текущему и, если стал, выводим
+             *MessageBox*/
+            if (Process.GetCurrentProcess().Id == GetParentProcessId(proc.Id))
+                MessageBox.Show(proc.ProcessName + " действительно дочерний процесс текущего процесса!");
+            /*указываем, что процесс должен генерировать события*/
+
+            proc.EnableRaisingEvents = true;
+            //добавляем обработчик на событие завершения процесса
+            proc.Exited += proc_Exited;
+            /*устанавливаем новый текст главному окну
+             *дочернего процесса*/
+            SetChildWindowText(proc.MainWindowHandle, "Child process #" + (++Counter));
+            /*проверяем, запускали ли мы экземпляр такого
+            *приложения и, если нет, то добавляем в список
+            *запущенных приложений*/
+            if (!StartedAssemblies.Items.Contains(proc.ProcessName))
+                StartedAssemblies.Items.Add(proc.ProcessName);
+            /*убираем приложение из списка доступных
+             *приложений*/
+            AvailableAssemblies.Items.Remove(AvailableAssemblies.SelectedItem);
+        }
+        /*метод, получающий PID родительского процесса
+*(использует WMI)*/
+        int GetParentProcessId(int Id)
+        {
+            int parentId = 0;
+            using (ManagementObject obj =
+            new ManagementObject("win32_process.handle=" +
+            Id.ToString()))
+            {
+                obj.Get();
+                parentId = Convert.
+                ToInt32(obj["ParentProcessId"]);
+            }
+            return parentId;
+        }
+
+        /*метод обёртывания для отправки сообщения
+        *WM _ SETTEXT*/
+        void SetChildWindowText(IntPtr Handle, string text)
+        {
+            SendMessage(Handle, WM_SETTEXT, 0, text);
+        }
+
+        /*обработчик события Exited класса Process*/
+        void proc_Exited(object sender, EventArgs e)
+        {
+            Process proc = sender as Process;
+            ////убираем процесс из списка запущенных //приложений
+            //StartedAssemblies.Items.Remove(proc.ProcessName);
+            //убираем процесс из списка дочерних процессов
+            Processes.Remove(proc);
+            //уменьшаем счётчик дочерних процессов на 1
+            Counter--;
+            int index = 0;
+            /*меняем текст для главных окон всех дочерних *процессов*/
+            foreach (var p in Processes)
+                SetChildWindowText(p.MainWindowHandle, "Child process #" + ++index);
+        }
+
+        //объявление делегата, принимающего параметр типа
+        //Process
+        delegate void ProcessDelegate(Process proc);
+        /*метод, который выполняет проход по всем дочерним
+        *процессам с заданым именем и выполняющий для этих
+        *процессов заданый делегатом метод*/
+        void ExecuteOnProcessesByName(string ProcessName, ProcessDelegate func)
+        {
+            /*получаем список, запущенных в операционной
+             *системе процессов*/
+            Process[] processes = Process.GetProcessesByName(ProcessName);
+            foreach (var process in processes)
+                /*если PID родительского процесса равен PID
+                 *текущего процесса*/
+                if (Process.GetCurrentProcess().Id == GetParentProcessId(process.Id))
+                    //запускаем метод
+                    func(process);
+        }
+
+        void Kill(Process proc)
+        {
+            proc.Kill();
+        }
+
+        void CloseMainWindow(Process proc)
+        {
+            proc.CloseMainWindow();
+        }
+
+        void RefreshMethod(Process proc)
+        {
+            proc.Refresh();
+        }
+
+        private void StartButton_Click(object sender, EventArgs e)
+        {
+            RunProcess(AvailableAssemblies.SelectedItem.ToString());
+        }
+
+        private void StopButton_Click(object sender, EventArgs e)
+        {
+            string nameProcc = StartedAssemblies.SelectedItem.ToString();
+            ExecuteOnProcessesByName(StartedAssemblies.SelectedItem.ToString(), Kill);
+            StartedAssemblies.Items.Remove(nameProcc);
+            AvailableAssemblies.Items.Add(nameProcc);
+
+        }
+
+        private void CloseWindowButton_Click(object sender, EventArgs e)
+        {
+            ExecuteOnProcessesByName(StartedAssemblies.SelectedItem.ToString(), CloseMainWindow);
+            StartedAssemblies.Items.Remove(StartedAssemblies.SelectedItem);
+        }
+
+        private void RefreshButton_Click(object sender, EventArgs e)
+        {
+            ExecuteOnProcessesByName(StartedAssemblies.SelectedItem.ToString(), RefreshMethod);
+        }
+
+        private void RunNotepadButton_Click(object sender, EventArgs e)
+        {
+            RunProcess("notepad.exe");
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            foreach (var proc in Processes)
+                proc.Kill();
+        }
+
+        private void StartedAssemblies_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (StartedAssemblies.SelectedItems.Count == 0)
+            {
+                StopButton.Enabled = false;
+                //CloseButton.Enabled = false;
+                CloseWindowButton.Enabled = false;
+            }
+            else
+            {
+                StopButton.Enabled = true;
+                //CloseButton.Enabled = true;
+                CloseWindowButton.Enabled = true;
+            }
+        }
+
+        private void AvailableAssemblies_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (AvailableAssemblies.SelectedItems.Count == 0)
+                StartButton.Enabled = false;
+            else
+                StartButton.Enabled = true;
+        }
+    }
+}
